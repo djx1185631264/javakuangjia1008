3c53caf3f898753ad5fc922b9818c7e7d3ef7547
djx <djx1185631264@163.com>
2021-12-03 16:20:52 +0800

    增加车的接口类

---
+++ Car.java
@@ -0,0 +1,20 @@
+package com.openlab.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    private Integer id;
+    private String name;
+    private float price;
+    //东八区
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createDate;
+}
